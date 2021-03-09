92c5ee0433f4f41a5425761ded66faba50e3bae3
sabrinapea <sabrinapea@yahoo.com>
2021-03-08 22:35:09 -0800

    Step 2

--- beginning/js/01-functions-practice.js
+++ beginning/js/01-functions-practice.js
@@ -1,11 +1,17 @@
 //STEP 1
-function halfNumber(number) {
-    let y = number / 2;
-    window.console.log("Half of " + number + " is " + y)
-}
-halfNumber(100);
+// function halfNumber(number) {
+//     let y = number / 2;
+//     window.console.log("Half of " + number + " is " + y)
+// }
+// halfNumber(100);
 
 //STEP 2
+function squareNumber(number) {
+  let y = number * number;
+  window.console.log("The result of squaring the number " + 
+  number + " is " + y);
+}
+squareNumber(5)
 
 //STEP 3
 
