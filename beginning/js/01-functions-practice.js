7182932135d46f88f26cfc719cb83512f6ed5737
sabrinapea <sabrinapea@yahoo.com>
2021-03-08 22:58:07 -0800

    step4 ex

--- beginning/js/01-functions-practice.js
+++ beginning/js/01-functions-practice.js
@@ -13,13 +13,18 @@
 // squareNumber(3);
 
 //STEP 3
-function percentOf(num1, num2) {
-    let y = (num1 / 100) * num2;
-    window.console.log(y + " is " + num1 + "% of " + num2);
-}
-percentOf(50, 4);
+// function percentOf(num1, num2) {
+//     let y = (num1 / 100) * num2;
+//     window.console.log(y + " is " + num1 + "% of " + num2);
+// }
+// percentOf(50, 4);
 
 
 //STEP 4
+function findModulus(num1, num2) {
+    let y = num2 % num1;
+    window.console.log(y + ' is the modulus of ' + num1 + ' and ' + num2);
+}
+findModulus(4, 10);
 
 //STEP 5
