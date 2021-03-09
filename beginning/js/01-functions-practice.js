379eb918e63ea16b80986d634512c0c1cd7ea3da
sabrinapea <sabrinapea@yahoo.com>
2021-03-08 22:49:00 -0800

    step3 ex

--- beginning/js/01-functions-practice.js
+++ beginning/js/01-functions-practice.js
@@ -6,14 +6,19 @@
 // halfNumber(100);
 
 //STEP 2
-function squareNumber(number) {
-  let y = number * number;
-  window.console.log("The result of squaring the number " + 
-  number + " is " + y);
-}
-squareNumber(5)
+// function squareNumber(number) {
+//   let y = number * number;
+//   window.console.log("The result of squaring the number " + number + " is " + y);
+// }
+// squareNumber(3);
 
 //STEP 3
+function percentOf(num1, num2) {
+    let y = (num1 / 100) * num2;
+    window.console.log(y + " is " + num1 + "% of " + num2);
+}
+percentOf(50, 4);
+
 
 //STEP 4
 
