1ef71ebc662d18e503ebad3598949c9da105dbd2
sabrinapea <sabrinapea@yahoo.com>
2021-03-09 00:16:55 -0800

    step5-exercise

--- beginning/js/01-functions-practice.js
+++ beginning/js/01-functions-practice.js
@@ -21,10 +21,29 @@
 
 
 //STEP 4
-function findModulus(num1, num2) {
-    let y = num2 % num1;
-    window.console.log(y + ' is the modulus of ' + num1 + ' and ' + num2);
-}
-findModulus(4, 10);
+// function findModulus(num1, num2) {
+//     let y = num2 % num1;
+//     window.console.log(y + ' is the modulus of ' + num1 + ' and ' + num2);
+// }
+// findModulus(4, 10);
 
 //STEP 5
+// function calNum(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < arguments[0].length; i++) {
+//         sum += parseInt(arguments[0][i]);
+
+//     }
+//     console.log(sum);
+// }
+// calNum(prompt(`Enter a string of numbers separated by commas`).split(","));
+
+function calNum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += parseInt(arguments[i]);
+    }
+    console.log(sum);
+}
+let input = prompt(`Enter three numbers separated by commas`).split(",");
+calNum(input[0],input[1],input[2]);
